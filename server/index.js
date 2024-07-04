9eb4c517cbe985fd9f4048cbdba6fa918e652a9d
Arif Basha K. <arifkolimi08@gmail.com>
2024-07-04 10:06:34 +0530

    First Commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const app = express()
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 app.get("/", (req, res) => {
     res.status(200).send(`<h1>Hello from Express</h1>`)
 })
 
 const PORT = process.env.PORT
-app.listen(PORT)
+app.listen(PORT, () => console.log(`Server is running`))
